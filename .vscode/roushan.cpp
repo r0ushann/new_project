36d85b5eb8af3fc9bcc94a6078b1766494852511
r0ushann <roushan007kr@gmail.com>
2022-01-01 13:51:32 +0530

    newgit

---
+++ .vscode/roushan.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std ;
+// int main(){
+//     cout<<"roushan";
+//     return;
+// }
+// Your First C++ Program
+
+#include<iostream>
+using namespace std;
+
+int main() {
+cout<< "roushan007";
+    return 0;
+}
